b562c131f79fb1e2fcfc2e3f715df894397b32cd
Dimitar Bonev <dbonev91@users.noreply.github.com>
2014-10-09 02:16:32 +0300

    Create IArmor.cs

---
+++ Lockheed_Inventory/Slots/Interfaces/IArmor.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+namespace Lockheed_Inventory.Slots.Interfaces
+{
+    public interface IArmor
+    {
+        List<Item> ArmorList { get; set; }
+    }
+}
