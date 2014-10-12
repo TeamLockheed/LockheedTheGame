1411dad3b34fcbeb10128bcd2b0ce53b76ca05bb
Borislav Kosharov <bosakmaw@gmail.com>
2014-10-12 15:23:33 +0300

    Added the true core project

---
+++ Lockheed/LockheedCore/SomeClass.cs
@@ -0,0 +1,12 @@
+﻿namespace LockheedCore
+{
+    using System;
+
+    public class SomeClass
+    {
+        public SomeClass()
+        {
+            throw new NotImplementedException("This is an example, just add your classes in this project");
+        }
+    }
+}
