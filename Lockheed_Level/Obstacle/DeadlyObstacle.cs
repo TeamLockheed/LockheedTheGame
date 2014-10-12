efe3885551f73516cdbaee30678df04c8766bd7d
glavunski <xiii1994@abv.bg>
2014-10-12 22:45:44 +0300

    Level class

---
+++ Lockheed_Level/Obstacle/DeadlyObstacle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LockHeedCore
+{
+    public class DeadlyObstacle : Obstacle
+    {
+        public DeadlyObstacle(string sourceImage,float x, float y)
+            :base(sourceImage,x,y)
+        {
+
+        }
+
+
+
+    }
+}
