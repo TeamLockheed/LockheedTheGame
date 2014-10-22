cc712aed00b873a2fa252648d59390e18137e092
Dimitar Bonev <dbonev91@users.noreply.github.com>
2014-10-23 01:58:21 +0300

    Update Level.cs

--- Lockheed/LockheedCore/Level.cs
+++ Lockheed/LockheedCore/Level.cs
@@ -1,22 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-
-
 namespace LockHeedCore
 {
+    using System;
+    using System.Collections.Generic;
+
+    using Exceptions;
+
     using SFML.Audio;
     using SFML.Window;
     using SFML.Graphics;
 
-    public enum DoorPosition { Left, Right, Top, Bottom };
+    public enum DoorPosition
+    {
+        Left,
+        Right,
+        Top,
+        Bottom
+    };
+
     public class Level
     {
-        public string Name { get; set; }
-        public string Background { get; set; }
-        public List<Obstacle> Obstacles { get; set; }
-        public List<Door> Doors { get; set; }
+        private string name;
+        private string background;
+        private List<Obstacle> obstacles;
+        private List<Door> doors; 
 
-        public Level(string name,string background,List<Obstacle> obstacles,List<Door> doors)
+        public Level(string name, string background, List<Obstacle> obstacles, List<Door> doors)
         {
             this.Name = name;
             this.Background = background;
@@ -24,14 +32,15 @@ namespace LockHeedCore
             this.Doors = doors;
         }
 
-        public static void DrawLevel(Level level,RenderWindow window) 
+        public static void DrawLevel(Level level, RenderWindow window)
         {
             RectangleShape background = new RectangleShape();
+
             background.Position = new Vector2f(0, 0);
             background.Size = new Vector2f(800, 450);
             background.Texture = new Texture(level.Background);
-
             window.Draw(background);
+
             foreach (var obstacle in level.Obstacles)
             {
                 Sprite obstSprite = new Sprite();
@@ -39,6 +48,7 @@ namespace LockHeedCore
                 obstSprite.Position = new Vector2f(obstacle.X, obstacle.Y);
                 window.Draw(obstSprite);
             }
+
             foreach (var door in level.Doors)
             {
                 Sprite doorSprite = new Sprite();
@@ -46,9 +56,58 @@ namespace LockHeedCore
                 doorSprite.Position = new Vector2f(door.X, door.Y);
                 window.Draw(doorSprite);
             }
-
         }
 
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                ExceptionsHolder.CheckNullableString(value, "Name cannot be null or empty!");
+                this.name = value;
+            }
+        }
 
+        public string Background
+        {
+            get
+            {
+                return this.background;
+            }
+            set
+            {
+                ExceptionsHolder.CheckNullableString(value, "BackGround cannot be null or empty!");
+                this.background = value;
+            }
+        }
+
+        public List<Obstacle> Obstacles
+        {
+            get
+            {
+                return this.obstacles;
+            }
+            set
+            {
+                ExceptionsHolder.CheckEmptyCollections(value, "Something wrong! There arent any obstacles!");
+                this.obstacles = value;
+            }
+        }
+
+        public List<Door> Doors
+        {
+            get
+            {
+                return this.doors;
+            }
+            set
+            {
+                ExceptionsHolder.CheckEmptyCollections(value, "Something wrong! There arent any doors!");
+                this.doors = value;
+            }
+        }
     }
 }
