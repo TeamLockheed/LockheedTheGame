5d137bbb71472a0038da01525e9f8febad8ddea1
Dimitar Bonev <dbonev91@users.noreply.github.com>
2014-10-23 01:36:11 +0300

    Update Door.cs

--- Lockheed/LockheedCore/Door.cs
+++ Lockheed/LockheedCore/Door.cs
@@ -1,24 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace LockHeedCore
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    
     public class Door
     {
-        public DoorPosition Position { get; set; }
-        public string PathTo { get; set; }
-        public string SourceImage { get; set; }
-
-        public Door(DoorPosition position,string pathTo,string sourceImage)
+        private DoorPosition position;
+        private string pathTo;
+        private string sourceImage;
+        
+        public Door(DoorPosition position, string pathTo, string sourceImage)
         {
             this.Position = position;
             this.PathTo = pathTo;
             this.SourceImage = sourceImage;
         }
-
+        
         public float X
         {
             get
@@ -27,13 +27,14 @@ namespace LockHeedCore
                 {
                     return 300;
                 }
-                else 
+                else
                 {
                     return 200;
                 }
             }
             set { this.X = value; }
         }
+        
         public float Y
         {
             get
@@ -48,6 +49,52 @@ namespace LockHeedCore
                 }
             }
             set { this.Y = value; }
+        }
+
+        public DoorPosition Position
+        {
+            get
+            {
+                return this.position;
+            }
+            set
+            {
+                this.position = value;
+            }
+        }
+
+        public string PathTo
+        {
+            get
+            {
+                return this.pathTo;
+            }
+            set
+            {
+                this.CheckNullableString(value, "Wrong path!");
+                this.pathTo = value;
+            }
+        }
+
+        public string SourceImage
+        {
+            get
+            {
+                return this.sourceImage;
+            }
+            set
+            {
+                this.CheckNullableString(value, "Wrong Image Source!");
+                this.sourceImage = value;
+            }
+        }
+
+        private void CheckNullableString(string value, string message)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException(message);
+            }
         }
     }
 }
