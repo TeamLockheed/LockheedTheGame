8e9eb6c7d21a25af6c2799a51b9b74e5197cea7a
Dimitar Bonev <dbonev91@users.noreply.github.com>
2014-10-23 03:13:18 +0300

    Update Obstacle.cs

--- Lockheed/LockheedCore/Obstacle/Obstacle.cs
+++ Lockheed/LockheedCore/Obstacle/Obstacle.cs
@@ -1,19 +1,57 @@
-﻿using System;
-
-
 namespace LockHeedCore
 {
+    using LockHeedCore.Exceptions;
+    using System;
     public abstract class Obstacle
     {
-        public string SourceImage { get; set; }
-        public float X { get; set; }
-        public float Y { get; set; }
+        private string sourceImage;
+        private float x;
+        private float y;
 
-        public Obstacle(string sourceImage,float x, float y)
+        public Obstacle(string sourceImage, float x, float y)
         {
             this.SourceImage = sourceImage;
             this.X = x;
             this.Y = y;
+        }
+
+        public string SourceImage
+        {
+            get
+            {
+                return this.sourceImage;
+            }
+            set
+            {
+                ExceptionsHolder.CheckNullableString(value, "SourceImage cannot be null or empty!");
+                this.sourceImage = value;
+            }
+        }
+
+        public float X
+        {
+            get
+            {
+                return this.x;
+            }
+            set
+            {
+                ExceptionsHolder.CheckNumberOutOfRangeMinValue(value, 0, "X cannot be less than 0!");
+                this.x = value;
+            }
+        }
+
+        public float Y
+        {
+            get
+            {
+                return this.y;
+            }
+            set
+            {
+                ExceptionsHolder.CheckNumberOutOfRangeMinValue(value, 0, "Y cannot be less than 0!");
+                this.y = value;
+            }
         }
     }
 }
