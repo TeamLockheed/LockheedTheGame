04fafa28349abdbd36140a9f4db7039dc2fa82ad
Dimitar Bonev <dbonev91@users.noreply.github.com>
2014-10-23 03:02:00 +0300

    Update Level.cs

--- Lockheed/LockheedCore/Level/Level.cs
+++ Lockheed/LockheedCore/Level/Level.cs
@@ -1,24 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using SFML.Graphics;
+using LockHeedCore.Exceptions;
+
 namespace LockHeedCore
 {
+    using System;
+    using System.Collections.Generic;
+
+    using System.Text;
+    using SFML.Graphics;
 
     public class Level : IDrawable
     {
-        public int Id { get; set; }
+        private int id;
+        private List<Obstacles> obstacles;
         public SpriteSheet SpriteSheet { get; set; }
-        public List<Obstacle> Obstacles { get; set; }
         public List<Door> Doors = new List<Door>(2);
         public List<Enemy> Enemies { get; set; }
 
-        public Level(int id,List<Obstacle> obstacles)
+        public Level(int id, List<Obstacle> obstacles)
         {
             this.Id = id;
             this.Obstacles = obstacles ?? new List<Obstacle>();
             this.SpriteSheet = new SpriteSheet(new Sprite(new Texture("level/baseLevel.png")));
         }
+
         public void checkDoors()
         {
             if (this.Enemies == null)
@@ -31,24 +35,47 @@ namespace LockHeedCore
         }
 
         public void Draw(RenderTarget window)
-        { 
-           window.Draw(this.SpriteSheet.CurrentSprite);
-            foreach(var door in this.Doors)
+        {
+            window.Draw(this.SpriteSheet.CurrentSprite);
+            foreach (var door in this.Doors)
             {
-               door.Draw(window);
+                door.Draw(window);
             }
-            foreach(var obstacle in this.Obstacles)
+            foreach (var obstacle in this.Obstacles)
             {
-               obstacle.Draw(window);
+                obstacle.Draw(window);
             }
         }
 
         public override string ToString()
         {
-
             return String.Join(", ", this.Doors.ToString());
-           
         }
 
+        public int Id
+        {
+            get
+            {
+                return this.id;
+            }
+            set
+            {
+                ExceptionsHolder.CheckNumberOutOfRangeMinValue(float.Parse(value.ToString()), 0, "Id cannot be negative!");
+                this.id = value;
+            }
+        }
+
+        public List<Obstacle> Obstacles
+        {
+            get
+            {
+                return this.obstacles;
+            }
+            set
+            {
+                ExceptionsHolder.CheckEmptyCollections(value, "Something wrong! There are no obstacles!");
+                this.obstacles = value;
+            }
+        }
     }
 }
