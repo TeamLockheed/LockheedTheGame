ef88397892d6b956a96594bd688fad20614cbd5d
Dimitar Bonev <dbonev91@users.noreply.github.com>
2014-10-23 03:39:55 +0300

    Update SpriteSheet.cs

--- Lockheed/LockheedCore/Sprites/SpriteSheet.cs
+++ Lockheed/LockheedCore/Sprites/SpriteSheet.cs
@@ -1,26 +1,23 @@
-﻿using System;
-using SFML.Graphics;
-
-
 namespace LockHeedCore
 {
-   public class SpriteSheet
-    {
-       public Sprite CurrentSprite { get; set; }
-       public Sprite FrontSprite { get; set; }
-       public Sprite BackSprite { get; set; }
-       public Sprite LeftSprite { get; set; }
-       public Sprite RightSprite { get; set; }
+    using System;
+    using SFML.Graphics;
 
-       public SpriteSheet(Sprite currentSprite, Sprite frontSprite = null, Sprite backSprite = null, Sprite leftSprite = null, Sprite rightSprite = null)
-       {
-           this.CurrentSprite = currentSprite;
-           this.FrontSprite = frontSprite;
-           this.BackSprite = backSprite;
-           this.LeftSprite = leftSprite;
-           this.RightSprite = rightSprite;
-       
-       }
-     
+    public class SpriteSheet
+    {
+        public Sprite CurrentSprite { get; set; }
+        public Sprite FrontSprite { get; set; }
+        public Sprite BackSprite { get; set; }
+        public Sprite LeftSprite { get; set; }
+        public Sprite RightSprite { get; set; }
+
+        public SpriteSheet(Sprite currentSprite, Sprite frontSprite = null, Sprite backSprite = null, Sprite leftSprite = null, Sprite rightSprite = null)
+        {
+            this.CurrentSprite = currentSprite;
+            this.FrontSprite = frontSprite;
+            this.BackSprite = backSprite;
+            this.LeftSprite = leftSprite;
+            this.RightSprite = rightSprite;
+        }
     }
 }
