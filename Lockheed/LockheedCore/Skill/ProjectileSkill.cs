4c37b263c70176fec4b587b3d84dba305344fb8e
glavunski <xiii1994@abv.bg>
2014-10-23 13:16:04 +0300

    Update

--- Lockheed/LockheedCore/Skill/ProjectileSkill.cs
+++ Lockheed/LockheedCore/Skill/ProjectileSkill.cs
@@ -18,10 +18,13 @@ namespace LockHeedCore
             if (character.Stats.Mana >= this.ManaCost)
             {
                 character.Stats.DecreaseMana(this.ManaCost);
-                var rad = Math.Atan2(character.SpriteSheet.CurrentSprite.Position.Y - mouseY, character.SpriteSheet.CurrentSprite.Position.X - mouseX);
-                var deltaX = Math.Cos(rad);
-                var deltaY = Math.Sin(rad);
-                Projectile Projectile = new Projectile("testDirectory", character.SpriteSheet.CurrentSprite.Position.X, character.SpriteSheet.CurrentSprite.Position.Y, deltaX, deltaY, ProjectileSpeed);
+                
+                var rad = Math.Atan2(mouseY - (character.BoundingBox.Top), mouseX - (character.BoundingBox.Left));
+                var deltaX = (float)Math.Cos(rad);
+                var deltaY = (float)Math.Sin(rad);
+                Sprite projSprite = new Sprite(new Texture(this.Directory));
+                projSprite.Rotation = (float)(rad*180/Math.PI);
+                Projectile Projectile = new Projectile(new SpriteSheet(projSprite), character.BoundingBox.Left, character.BoundingBox.Top-10, deltaX, deltaY, Program.MovementModifier);
             }
         }
 
