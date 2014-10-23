639b7231d5f9a8861dc6bf740153ed0b60d2f567
Dimitar Bonev <dbonev91@users.noreply.github.com>
2014-10-23 03:20:43 +0300

    Update Effect.cs

--- Lockheed/LockheedCore/Skill/Effect/Effect.cs
+++ Lockheed/LockheedCore/Skill/Effect/Effect.cs
@@ -1,20 +1,59 @@
-﻿using System;
-
 namespace LockHeedCore
 {
+    using System;
+
+    using LockHeedCore.Exceptions;
+
     public abstract class Effect
     {
+        private string directory;
+        private double x;
+        private double y;
 
-        public string Directory;
-        public double X;
-        public double Y;
-
-        public Effect(string directory, double x, double y) 
+        public Effect(string directory, double x, double y)
         {
             this.Directory = directory;
             this.X = x;
             this.Y = y;
         }
-        
+
+        public string Directory
+        {
+            get
+            {
+                return this.directory;
+            }
+            set
+            {
+                ExceptionsHolder.CheckNullableString(value, "Directory cannot be null or empty!");
+                this.directory = value;
+            }
+        }
+
+        public double X
+        {
+            get
+            {
+                return this.x;
+            }
+            set
+            {
+                ExceptionsHolder.CheckNumberOutOfRangeMinValue(float.Parse(value.ToString()), 0, "X cannot be less than 0!");
+                this.x = value;
+            }
+        }
+
+        public double Y
+        {
+            get
+            {
+                return this.y;
+            }
+            set
+            {
+                ExceptionsHolder.CheckNumberOutOfRangeMinValue(float.Parse(value.ToString()), 0, "Y cannot be less than 0!");
+                this.y = value;
+            }
+        }
     }
 }
